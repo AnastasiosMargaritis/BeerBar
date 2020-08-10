1a957e76040e8bbdf55fcff76fb8c56cb862f136
AnastasiosMargaritis <58393884+AnastasiosMargaritis@users.noreply.github.com>
2020-08-10 22:32:18 +0300

    Create BeerController

---
+++ Beer-Bar/src/main/java/com/beer/controller/BeerController.java
@@ -0,0 +1,30 @@
+package com.beer.controller;
+
+import com.beer.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping
+public class BeerController {
+
+    @GetMapping
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    
+}
