fbef312a51ecb4daaefe85b550cc34d8468fcb97
AnastasiosMargaritis <58393884+AnastasiosMargaritis@users.noreply.github.com>
2020-08-11 14:40:39 +0300

    Bean Validation.
    Adding validation to Beer Entity.

--- Beer-Bar/src/main/java/com/beer/web/model/BeerDto.java
+++ Beer-Bar/src/main/java/com/beer/web/model/BeerDto.java
@@ -1,10 +1,14 @@
 package com.beer.web.model;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -16,18 +20,30 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyle;
 
+    @NotNull
+    @Positive
     private Long upc;
 
+    @NotNull
+    @Positive
     private BigDecimal price;
 
     private Integer quantityOnHand;
